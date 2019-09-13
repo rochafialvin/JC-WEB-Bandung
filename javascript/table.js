011b097dfe6c844ca079464663bb9dcc24aa0e14
rochafialvin <rochafi.dev@gmail.com>
2019-09-13 11:20:11 +0700

    Success input

--- javascript/table.js
+++ javascript/table.js
@@ -5,16 +5,24 @@ let arrManusia = []
 // Akan running ketika menekan tombol 'Tambahkan' pada Table Manusia
 var inputManusia = () => {
     // Ambil data dari form
-    // let nama = document.getElementById('nama').value
+    let nama = document.getElementById('nama').value
     let umur = document.getElementById('umur').value
-    let NAMA = document.querySelector('input[name=gender]:checked').value
+    let gender = document.querySelector('input[name=gender]:checked').value
+    let pekerjaan = document.getElementById('pekerjaan').value
+
     /*
         querySelector hanya akan mencari satu element, jika sudah menemukannya, tidak akan mencari lagi
         querySelectorAll akan mencari semua element
-        */
-
-    console.log(NAMA)
+    */
 
     // Tambahkan data ke array
+
+    arrManusia.push(
+        {
+            nama, umur, gender,pekerjaan
+        }
+    )
+
+    console.log(arrManusia)
 
 }
